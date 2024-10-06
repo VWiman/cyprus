2fdd224774267b88c99fbd90c4f89ece9ee98889
why_code <119089018+VWiman@users.noreply.github.com>
2024-10-06 21:23:46 +0200

    före rebase

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 
+// Lite kommentar inför rebase
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
