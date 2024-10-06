8185e9abbfb973a9d328cefe9954a83b5af38ee1
why_code <119089018+VWiman@users.noreply.github.com>
2024-10-06 21:33:14 +0200

    Add new comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,14 @@ function Home() {
 	const handleLoginClick = () => {
 		navigate("/login");
 	};
-
+	// I added this text while learning to rebase
 	return (
 		<div className="flex flex-col bg-white/50 rounded-sm gap-4 shadow-lg backdrop-blur-md">
 			<h2 className="rounded-t-sm bg-orange-400/80 p-2 text-white font-semibold text-4xl">Welcome to Cyprus Club!</h2>
 			<div className="flex w-full items-center">
-				<p className="text-orange-500/80 text-xs pl-3 font-bold self-start">View your username and other cool features.</p>
+				<p className="text-orange-500/80 text-xs pl-3 font-bold self-start">
+					View your username and other cool features.
+				</p>
 				<button
 					className="bg-blue-400/80 text-white shadow-black/10 shadow-md hover:shadow-black/5 active:shadow-none text-sm font-semibold w-fit h-fit rounded-md ml-auto mb-5 mr-5"
 					onClick={handleLoginClick}>
